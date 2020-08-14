7be4f6a3cfa4c28897e6f1415fdb2385f88b2481
Anubhav Mishra <anubmish@adobe.com>
2020-08-14 13:19:48 +0530

    feat: Complete xylophone application
    This app works as a mini xylophone.
    
    It has keys speard throughout the screen which on tap
    
    plays a musical note.
    
    This app is a result of tutorials from the app brewery.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
+  Expanded buildKey(Color color, int noteNumber) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(noteNumber);
+          }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Center(
+              child: Column(
+//            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+              buildKey(Colors.red, 1),
+            ],
+          )),
         ),
       ),
     );
